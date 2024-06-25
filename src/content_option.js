c65c5cd6fe1d54572b7e02a41f05940e5947cea0
Damjan Okilj <damjan.okilj@studen-global.com>
2024-06-25 10:35:22 +0200

    edit

--- src/content_option.js
+++ src/content_option.js
@@ -134,7 +134,7 @@ const dataportfolio = [{
 const contactConfig = {
     YOUR_EMAIL: "name@domain.com",
     YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    description: "",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
     YOUR_SERVICE_ID: "service_id",
